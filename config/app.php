11c6032273fdbda09a1873804b070c0b53b9e563
luqmanulhakim96 <hakim9608@gmail.com>
2019-11-06 11:52:14 +0800

    miss a comma

--- config/app.php
+++ config/app.php
@@ -247,7 +247,7 @@ return [
         Webkul\Tax\Providers\TaxServiceProvider::class,
         Webkul\API\Providers\APIServiceProvider::class,
         Webkul\Discount\Providers\DiscountServiceProvider::class,
-        Webkul\CMS\Providers\CMSServiceProvider::class
+        Webkul\CMS\Providers\CMSServiceProvider::class,
 
 
         //selfmade packages
