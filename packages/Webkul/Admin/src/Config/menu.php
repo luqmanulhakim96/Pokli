6dfdc517cdcbdeb456e35bbbcbdd55d3721fec8e
Rizal <rizal@artaniscloud.com>
2019-11-07 17:17:09 +0800

    Ui remove demo purpose

--- packages/Webkul/Admin/src/Config/menu.php
+++ packages/Webkul/Admin/src/Config/menu.php
@@ -98,119 +98,119 @@ return [
         'sort' => 4,
         'icon-class' => ''
     ]
-    // , [
-    //     'key' => 'configuration',
-    //     'name' => 'admin::app.layouts.configure',
-    //     'route' => 'admin.configuration.index',
-    //     'sort' => 7,
-    //     'icon-class' => 'configuration-icon'
-    // ], [
-    //     'key' => 'settings',
-    //     'name' => 'admin::app.layouts.settings',
-    //     'route' => 'admin.locales.index',
-    //     'sort' => 6,
-    //     'icon-class' => 'settings-icon'
-    // ], [
-    //     'key' => 'settings.locales',
-    //     'name' => 'admin::app.layouts.locales',
-    //     'route' => 'admin.locales.index',
-    //     'sort' => 1,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'settings.currencies',
-    //     'name' => 'admin::app.layouts.currencies',
-    //     'route' => 'admin.currencies.index',
-    //     'sort' => 2,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'settings.exchange_rates',
-    //     'name' => 'admin::app.layouts.exchange-rates',
-    //     'route' => 'admin.exchange_rates.index',
-    //     'sort' => 3,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'settings.inventory_sources',
-    //     'name' => 'admin::app.layouts.inventory-sources',
-    //     'route' => 'admin.inventory_sources.index',
-    //     'sort' => 4,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'settings.channels',
-    //     'name' => 'admin::app.layouts.channels',
-    //     'route' => 'admin.channels.index',
-    //     'sort' => 5,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'settings.users',
-    //     'name' => 'admin::app.layouts.users',
-    //     'route' => 'admin.users.index',
-    //     'sort' => 6,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'settings.users.users',
-    //     'name' => 'admin::app.layouts.users',
-    //     'route' => 'admin.users.index',
-    //     'sort' => 1,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'settings.users.roles',
-    //     'name' => 'admin::app.layouts.roles',
-    //     'route' => 'admin.roles.index',
-    //     'sort' => 2,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'settings.sliders',
-    //     'name' => 'admin::app.layouts.sliders',
-    //     'route' => 'admin.sliders.index',
-    //     'sort' => 7,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'settings.taxes',
-    //     'name' => 'admin::app.layouts.taxes',
-    //     'route' => 'admin.tax-categories.index',
-    //     'sort' => 8,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'settings.taxes.tax-categories',
-    //     'name' => 'admin::app.layouts.tax-categories',
-    //     'route' => 'admin.tax-categories.index',
-    //     'sort' => 1,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'settings.taxes.tax-rates',
-    //     'name' => 'admin::app.layouts.tax-rates',
-    //     'route' => 'admin.tax-rates.index',
-    //     'sort' => 2,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'promotions',
-    //     'name' => 'admin::app.layouts.promotion',
-    //     'route' => 'admin.cart-rule.index',
-    //     'sort' => 5,
-    //     'icon-class' => 'promotion-icon'
-    // ], [
-    //     'key' => 'promotions.cart-rule',
-    //     'name' => 'admin::app.promotion.cart-rule',
-    //     'route' => 'admin.cart-rule.index',
-    //     'sort' => 1,
-    //     'icon-class' => ''
-    // ], [
-    //     'key' => 'promotions.catalog-rule',
-    //     'name' => 'admin::app.promotion.catalog-rule',
-    //     'route' => 'admin.catalog-rule.index',
-    //     'sort' => 1,
-    //     'icon-class' => '',
-    // ], [
-    //     'key' => 'cms',
-    //     'name' => 'admin::app.layouts.cms',
-    //     'route' => 'admin.cms.index',
-    //     'sort' => 5,
-    //     'icon-class' => 'cms-icon'
-    // ], [
-    //     'key' => 'cms.pages',
-    //     'name' => 'admin::app.cms.pages.pages',
-    //     'route' => 'admin.cms.index',
-    //     'sort' => 1,
-    //     'icon-class' => ''
-    // ]
+    , [
+        'key' => 'configuration',
+        'name' => 'admin::app.layouts.configure',
+        'route' => 'admin.configuration.index',
+        'sort' => 7,
+        'icon-class' => 'configuration-icon'
+    ], [
+        'key' => 'settings',
+        'name' => 'admin::app.layouts.settings',
+        'route' => 'admin.locales.index',
+        'sort' => 6,
+        'icon-class' => 'settings-icon'
+    ], [
+        'key' => 'settings.locales',
+        'name' => 'admin::app.layouts.locales',
+        'route' => 'admin.locales.index',
+        'sort' => 1,
+        'icon-class' => ''
+    ], [
+        'key' => 'settings.currencies',
+        'name' => 'admin::app.layouts.currencies',
+        'route' => 'admin.currencies.index',
+        'sort' => 2,
+        'icon-class' => ''
+    ], [
+        'key' => 'settings.exchange_rates',
+        'name' => 'admin::app.layouts.exchange-rates',
+        'route' => 'admin.exchange_rates.index',
+        'sort' => 3,
+        'icon-class' => ''
+    ], [
+        'key' => 'settings.inventory_sources',
+        'name' => 'admin::app.layouts.inventory-sources',
+        'route' => 'admin.inventory_sources.index',
+        'sort' => 4,
+        'icon-class' => ''
+    ], [
+        'key' => 'settings.channels',
+        'name' => 'admin::app.layouts.channels',
+        'route' => 'admin.channels.index',
+        'sort' => 5,
+        'icon-class' => ''
+    ], [
+        'key' => 'settings.users',
+        'name' => 'admin::app.layouts.users',
+        'route' => 'admin.users.index',
+        'sort' => 6,
+        'icon-class' => ''
+    ], [
+        'key' => 'settings.users.users',
+        'name' => 'admin::app.layouts.users',
+        'route' => 'admin.users.index',
+        'sort' => 1,
+        'icon-class' => ''
+    ], [
+        'key' => 'settings.users.roles',
+        'name' => 'admin::app.layouts.roles',
+        'route' => 'admin.roles.index',
+        'sort' => 2,
+        'icon-class' => ''
+    ], [
+        'key' => 'settings.sliders',
+        'name' => 'admin::app.layouts.sliders',
+        'route' => 'admin.sliders.index',
+        'sort' => 7,
+        'icon-class' => ''
+    ], [
+        'key' => 'settings.taxes',
+        'name' => 'admin::app.layouts.taxes',
+        'route' => 'admin.tax-categories.index',
+        'sort' => 8,
+        'icon-class' => ''
+    ], [
+        'key' => 'settings.taxes.tax-categories',
+        'name' => 'admin::app.layouts.tax-categories',
+        'route' => 'admin.tax-categories.index',
+        'sort' => 1,
+        'icon-class' => ''
+    ], [
+        'key' => 'settings.taxes.tax-rates',
+        'name' => 'admin::app.layouts.tax-rates',
+        'route' => 'admin.tax-rates.index',
+        'sort' => 2,
+        'icon-class' => ''
+    ], [
+        'key' => 'promotions',
+        'name' => 'admin::app.layouts.promotion',
+        'route' => 'admin.cart-rule.index',
+        'sort' => 5,
+        'icon-class' => 'promotion-icon'
+    ], [
+        'key' => 'promotions.cart-rule',
+        'name' => 'admin::app.promotion.cart-rule',
+        'route' => 'admin.cart-rule.index',
+        'sort' => 1,
+        'icon-class' => ''
+    ], [
+        'key' => 'promotions.catalog-rule',
+        'name' => 'admin::app.promotion.catalog-rule',
+        'route' => 'admin.catalog-rule.index',
+        'sort' => 1,
+        'icon-class' => '',
+    ], [
+        'key' => 'cms',
+        'name' => 'admin::app.layouts.cms',
+        'route' => 'admin.cms.index',
+        'sort' => 5,
+        'icon-class' => 'cms-icon'
+    ], [
+        'key' => 'cms.pages',
+        'name' => 'admin::app.cms.pages.pages',
+        'route' => 'admin.cms.index',
+        'sort' => 1,
+        'icon-class' => ''
+    ]
 ];
