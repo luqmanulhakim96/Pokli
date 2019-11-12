76bf71c2f2300391236be6947e8b01f47cab7c6e
luqmanulhakim96 <hakim9608@gmail.com>
2019-11-12 10:56:52 +0800

    Update pull_gold_rates.php

--- pull_gold_rates.php
+++ pull_gold_rates.php
@@ -191,13 +191,13 @@ INSERT INTO `goldrate` ( `currency_code`, `unit_type`,  `gold_rate`, `24karat_ra
 */
 
 
-$gmt_datetime_gold_updated_corrected = date("Y/m/d H:i:s", strtotime($gmt_datetime_gold_updated));
-$gmt_datetime_currency_updated_corrected = date("Y/m/d H:i:s", strtotime($gmt_datetime_currency_updated));
+// $gmt_datetime_gold_updated_corrected = date("Y/m/d H:i:s", strtotime($gmt_datetime_gold_updated));
+// $gmt_datetime_currency_updated_corrected = date("Y/m/d H:i:s", strtotime($gmt_datetime_currency_updated));
 
 $sql = "UPDATE goldrate 	SET  gold_rate='".$current_gold_price."',
-                                 gmt_datetime_gold_updated = '".$gmt_datetime_gold_updated_corrected."',
+                                 gmt_datetime_gold_updated = STR_TO_DATE('".$gmt_datetime_gold_updated."','%d-%m-%Y %h:%i:%s %p'),
                                  currency_rate ='".$currency_rate."',
-								                 gmt_datetime_currency_updated='".$gmt_datetime_currency_updated_corrected."',
+								                 gmt_datetime_currency_updated= STR_TO_DATE('".$gmt_datetime_currency_updated."','%d-%m-%Y %h:%i:%s %p'),
                                  24karat_rate ='".$currency_rate."',
                                  22karat_rate ='". ($currency_rate*0.916) ."',
                                  21karat_rate ='". ($currency_rate*0.875 )."',
