1adbd7b0912c8ffcc2c211e2de4fff6545d662b1
luqmanulhakim96 <hakim9608@gmail.com>
2019-11-15 11:53:10 +0800

    update gap and add sap

--- curl_sap_price.php
+++ curl_sap_price.php
@@ -30,14 +30,14 @@
 
   $xpath = new DOMXPath($doc);
 
-  ////////////////////////////////////////////////--CURL GOLD PRICE--////////////////////////////////////////////////
+  ////////////////////////////////////////////////--CURL SIVLER PRICE--////////////////////////////////////////////////
   // get the gold current price
-  $get_gap_price = "//div[@id='gap-div-table2']";
-  $get_gap_price_array = $xpath->query($get_gap_price);
-  $gold_price = $get_gap_price_array->item(0)->textContent;
+  $get_sap_price = "//div[@id='sap-div-table2']";
+  $get_sap_price_array = $xpath->query($get_sap_price);
+  $silver_price = $get_sap_price_array->item(0)->textContent;
 
   //get the gram for RM 100 and the price for a gram
-  list(, , , , , , $weight_for_a_hundread, , $price_for_a_gram, , ) = explode(' ', $gold_price);
+  list(, , , , , , $weight_for_a_hundread, , $price_for_a_gram, , ) = explode(' ', $silver_price);
 
   ////////////////////////////////////////////////--CURL DATETIME--////////////////////////////////////////////////
   // get the datetime for gold update
@@ -54,40 +54,37 @@
   //convert $date_and_time_substr to datetime for db compatibility
   $datetime_gold_updated = date("Y/m/d H:i:s", strtotime($date_and_time_substr));
 
+
   ////////////////////////////////////////////////--CURL 24K--////////////////////////////////////////////////
   // get the 24k gold current price
-  $get_24k_price = "//table[@id='goldbar-table-content2']";
+  $get_24k_price = "//table[@id='silverbar-table-content2']";
   $get_24k_price_array = $xpath->query($get_24k_price);
-  $gold_24k_price = $get_24k_price_array->item(0)->textContent;
+  $silver_24k_price = $get_24k_price_array->item(0)->textContent;
 
-  $gold_24k_price = preg_replace('/\s+/', '_', $gold_24k_price);
-  echo $gold_24k_price;
-  echo "<br>";
+  $silver_24k_price = preg_replace('/\s+/', '_', $silver_24k_price);
 
-  list(, , , , , , , , $sell_10g, $buy_10g, , , $sell_20g, $buy_20g, , , $sell_50g, $buy_50g, , , $sell_100g, $buy_100g, , , $sell_250g, $buy_250g, , ,$sell_1000g,$buy_1000g ) = explode('_', $gold_24k_price);
+
+  list(, , , , , , , , $sell_100g, $buy_100g, , , $sell_250g, $buy_250g, , , $sell_500g, $buy_500g, , , $sell_1000g, $buy_1000g, , , $sell_5000g, $buy_5000g) = explode('_', $silver_24k_price);
 
   //replace comma with whitespace
-  $buy_10g = preg_replace('/[,]+/', '', trim($buy_10g));
-  $sell_10g = preg_replace('/[,]+/', '', trim($sell_10g));
-
-  $buy_20g = preg_replace('/[,]+/', '', trim($buy_20g));
-  $sell_20g = preg_replace('/[,]+/', '', trim($sell_20g));
-
-  $buy_50g = preg_replace('/[,]+/', '', trim($buy_50g));
-  $sell_50g = preg_replace('/[,]+/', '', trim($sell_50g));
-
   $buy_100g = preg_replace('/[,]+/', '', trim($buy_100g));
   $sell_100g = preg_replace('/[,]+/', '', trim($sell_100g));
 
   $buy_250g = preg_replace('/[,]+/', '', trim($buy_250g));
   $sell_250g = preg_replace('/[,]+/', '', trim($sell_250g));
 
+  $buy_500g = preg_replace('/[,]+/', '', trim($buy_500g));
+  $sell_500g = preg_replace('/[,]+/', '', trim($sell_500g));
+
   $buy_1000g = preg_replace('/[,]+/', '', trim($buy_1000g));
   $sell_1000g = preg_replace('/[,]+/', '', trim($sell_1000g));
 
+  $buy_5000g = preg_replace('/[,]+/', '', trim($buy_5000g));
+  $sell_5000g = preg_replace('/[,]+/', '', trim($sell_5000g));
+
   ////////////////////////////////////////////////--SQL UPDATE--////////////////////////////////////////////////
-  //update gram value for per RM 100
-  $sql_update_gram_value = "UPDATE gold_live_price_gap SET gram ='".$weight_for_a_hundread."' WHERE price = '100'";
+  // update gram value for per RM 100
+  $sql_update_gram_value = "UPDATE silver_live_price_sap SET gram ='".$weight_for_a_hundread."' WHERE price = '100'";
 
   if ($conn->query($sql_update_gram_value) === TRUE) {
       echo "Gram per RM100 updated successfully <br>";
@@ -96,7 +93,7 @@
   }
 
   //update price per gram
-  $sql_update_price_value = "UPDATE gold_live_price_gap SET price ='".$price_for_a_gram."' WHERE gram = '1'";
+  $sql_update_price_value = "UPDATE silver_live_price_sap SET price ='".$price_for_a_gram."' WHERE gram = '1'";
 
   if ($conn->query($sql_update_price_value) === TRUE) {
       echo "Price per gram updated successfully <br>";
@@ -105,7 +102,7 @@
   }
 
   //update datetime
-  $sql_update_datetime = "UPDATE gold_live_price_gap SET last_updated ='".$datetime_gold_updated."'";
+  $sql_update_datetime = "UPDATE silver_live_price_sap SET last_updated ='".$datetime_gold_updated."'";
 
   if ($conn->query($sql_update_datetime) === TRUE) {
       echo "Date and time updated successfully <br>";
@@ -113,54 +110,48 @@
       echo "Error updating record: " . $conn->error;
   }
 
-  $sql_update_gram_value_10g = "UPDATE gold_live_price_24k SET buy ='".$buy_10g."', sell='".$sell_10g."' WHERE gram = '10'";
+  //update gram value for per RM 100
+  $sql_update_gram_value_100g = "UPDATE silver_live_price_24k SET buy ='".$buy_100g."', sell='".$sell_100g."' WHERE gram = '100'";
 
-  if ($conn->query($sql_update_gram_value_10g) === TRUE) {
-      echo "Gram 10g updated successfully <br>";
+  if ($conn->query($sql_update_gram_value_100g) === TRUE) {
+      echo "Gram 100g updated successfully <br>";
   } else {
       echo "Error updating record: " . $conn->error;
   }
 
   //update price per gram
-  $sql_update_price_value_20g = "UPDATE gold_live_price_24k SET buy ='".$buy_20g."', sell='".$sell_20g."' WHERE gram = '20'";
+  $sql_update_price_value_200g = "UPDATE silver_live_price_24k SET buy ='".$buy_250g."', sell='".$sell_250g."' WHERE gram = '250'";
 
-  if ($conn->query($sql_update_price_value_20g) === TRUE) {
-      echo "Price 20g updated successfully <br>";
-  } else {
-      echo "Error updating record: " . $conn->error;
-  }
-
-  $sql_update_price_value_50g = "UPDATE gold_live_price_24k SET buy ='".$buy_50g."', sell='".$sell_50g."' WHERE gram = '50'";
-
-  if ($conn->query($sql_update_price_value_50g) === TRUE) {
-      echo "Price 50g updated successfully <br>";
-  } else {
-      echo "Error updating record: " . $conn->error;
-  }
-
-  $sql_update_price_value_100g = "UPDATE gold_live_price_24k SET buy ='".$buy_100g."', sell='".$sell_100g."' WHERE gram = '100'";
-
-  if ($conn->query($sql_update_price_value_100g) === TRUE) {
-      echo "Price 100g updated successfully <br>";
-  } else {
-      echo "Error updating record: " . $conn->error;
-  }
-
-  $sql_update_price_value_250g = "UPDATE gold_live_price_24k SET buy ='".$buy_250g."', sell='".$sell_250g."' WHERE gram = '250'";
-
-  if ($conn->query($sql_update_price_value_250g) === TRUE) {
+  if ($conn->query($sql_update_price_value_200g) === TRUE) {
       echo "Price 250g updated successfully <br>";
   } else {
       echo "Error updating record: " . $conn->error;
   }
 
-  $sql_update_price_value_1000g = "UPDATE gold_live_price_24k SET buy ='".$buy_1000g."', sell='".$sell_1000g."' WHERE gram = '1000'";
+  $sql_update_price_value_500g = "UPDATE silver_live_price_24k SET buy ='".$buy_500g."', sell='".$sell_500g."' WHERE gram = '500'";
+
+  if ($conn->query($sql_update_price_value_500g) === TRUE) {
+      echo "Price 500g updated successfully <br>";
+  } else {
+      echo "Error updating record: " . $conn->error;
+  }
+
+  $sql_update_price_value_1000g = "UPDATE silver_live_price_24k SET buy ='".$buy_1000g."', sell='".$sell_1000g."' WHERE gram = '1000'";
 
   if ($conn->query($sql_update_price_value_1000g) === TRUE) {
       echo "Price 1000g updated successfully <br>";
   } else {
       echo "Error updating record: " . $conn->error;
   }
+
+  $sql_update_price_value_5000g = "UPDATE silver_live_price_24k SET buy ='".$buy_5000g."', sell='".$sell_5000g."' WHERE gram = '5000'";
+
+  if ($conn->query($sql_update_price_value_5000g) === TRUE) {
+      echo "Price 5000g updated successfully <br>";
+  } else {
+      echo "Error updating record: " . $conn->error;
+  }
+
 
   $conn->close();
 
