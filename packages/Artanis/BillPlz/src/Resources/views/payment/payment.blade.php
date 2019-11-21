229b3110b3da07b0e84fefe9c05037de75e27a36
luqmanulhakim96 <hakim9608@gmail.com>
2019-11-21 15:28:28 +0800

    Update payment.blade.php

--- packages/Artanis/BillPlz/src/Resources/views/payment/payment.blade.php
+++ packages/Artanis/BillPlz/src/Resources/views/payment/payment.blade.php
@@ -1,13 +1,13 @@
 <?php
-//
-// use Billplz\Client;
-//
-// $billplz = Client::make('155994cc-37ea-4c78-9460-1062df930f2c');
-// $billplz->useSandbox();
-// $collection = $billplz->collection();
-// $response = $collection->create('My First API Collection');
-//
-// var_dump($response->toArray());
+
+  use Billplz\Client;
+
+  $billplz = Client::make('155994cc-37ea-4c78-9460-1062df930f2c');
+  $billplz->useSandbox();
+  $collection = $billplz->collection();
+  $response = $collection->create('My First API Collection');
+
+  var_dump($response->toArray());
 ?>
 
 <!DOCTYPE html>
