68091a8ee8cfc7976207dcc7e9c6982126b7d929
luqmanulhakim96 <hakim9608@gmail.com>
2019-11-21 14:34:16 +0800

    Revert "Revert "Merge branch 'addModule'""
    This reverts commit 855b1460ff67405f23ba25242e09447a6d0a3306.

---
+++ packages/Artanis/BillPlz/src/Providers/BillPlzServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Artanis\BillPlz\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class BillPlzServiceProvider extends ServiceProvider
+{
+    /**
+    * Bootstrap services.
+    *
+    * @return void
+    */
+    public function boot()
+    {
+       include __DIR__ . '/../Http/routes.php';
+       $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'payment');
+    }
+
+    /**
+    * Register services.
+    *
+    * @return void
+    */
+    public function register()
+    {
+
+    }
+}
