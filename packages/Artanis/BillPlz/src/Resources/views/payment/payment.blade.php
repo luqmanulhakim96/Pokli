f8b1ec1017c065265f549382ca4eb7674da0e762
luqmanulhakim96 <hakim9608@gmail.com>
2019-11-21 14:50:14 +0800

    Revert "Revert "Revert "Revert "Merge branch 'addModule'""""
    This reverts commit a229f564583557de1b8da6ba3c20c1177943b957.

---
+++ packages/Artanis/BillPlz/src/Resources/views/payment/payment.blade.php
@@ -0,0 +1,22 @@
+<?php
+
+use Billplz\Client;
+
+$billplz = Client::make('155994cc-37ea-4c78-9460-1062df930f2c');
+$billplz->useSandbox();
+$collection = $billplz->collection();
+$response = $collection->create('My First API Collection');
+
+var_dump($response->toArray());
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    HELLO WORLD
+  </body>
+</html>
