082321e53e6cab2c29e53250780e28bd64474b15
luqmanulhakim96 <hakim9608@gmail.com>
2019-11-26 15:10:37 +0800

    add billplz payment method (test)

---
+++ packages/Artanis/BillPlz/src/Config/paymentmethod.php
@@ -0,0 +1,12 @@
+<?php
+
+  return [
+    'billplz' => [
+        'code' => 'billplz',
+        'title' => 'Bank Transfer (FPX)'.
+        'description' => 'BillPlz Payments',
+        'class' => 'Billplz\Laravel\Billplz',
+        'sandbox' => true,
+        'active' => true
+    ]
+];
---
+++ packages/Artanis/BillPlz/src/Config/system.php
@@ -0,0 +1,34 @@
+<?php
+
+  return [
+    [
+      'key' => 'sales',
+      'name' => 'Sales',
+      'sort' => 1
+    ], [
+      'key' => 'sales.paymentmethods',
+      'name' => 'Payment Methods',
+      'sort' => 2,
+    ], [
+      'key' =>'sales.paymentmethods.billplz',
+      'name' => 'Bank Transfer',
+      'sort' => 4,
+      'fields' => [
+        [
+          'name' => 'active',
+          'title' => 'Enable For Checkout',
+          'type' => 'select',
+          'options' => [
+            [
+              'title' => 'True',
+              'value' => true
+            ], [
+              'title' => 'False',
+              'value' => false
+            ]
+          ],
+          'validation' => 'required'
+        ]
+      ]
+    ]
+];
