7a49527bcfeb79c32cb166f50b7e5f1f2d7d1128
luqmanulhakim96 <hakim9608@gmail.com>
2019-11-28 17:42:08 +0800

    remove billplz package and added payment package

---
+++ packages/Artanis/Payment/src/Payment/BillPlz.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Artanis\Payment\Payment;
+
+/**
+ * Cash On Delivery payment method class
+ *
+ * @author    Jitendra Singh <jitendra@webkul.com>
+ * @copyright 2018 Webkul Software Pvt Ltd (http://www.webkul.com)
+ */
+class BillPlz extends Payment
+{
+    /**
+     * Payment method code
+     *
+     * @var string
+     */
+    protected $code  = 'billplz';
+
+    public function getRedirectUrl()
+    {
+
+    }
+}
--- packages/Webkul/Payment/src/Config/system.php
+++ packages/Webkul/Payment/src/Config/system.php
@@ -190,13 +190,55 @@ return [
           'title' => 'Enable For Checkout',
           'type' => 'select',
           'options' => [
-            [
-              'title' => 'True',
-              'value' => true
-            ], [
-              'title' => 'False',
-              'value' => false
-            ]
+              [
+                  'name' => 'title',
+                  'title' => 'admin::app.admin.system.title',
+                  'type' => 'text',
+                  'validation' => 'required',
+                  'channel_based' => false,
+                  'locale_based' => true
+              ], [
+                  'name' => 'description',
+                  'title' => 'admin::app.admin.system.description',
+                  'type' => 'textarea',
+                  'channel_based' => false,
+                  'locale_based' => true
+              ], [
+                  'name' => 'active',
+                  'title' => 'admin::app.admin.system.status',
+                  'type' => 'select',
+                  'options' => [
+                      [
+                          'title' => 'Active',
+                          'value' => true
+                      ], [
+                          'title' => 'Inactive',
+                          'value' => false
+                      ]
+                  ],
+                  'validation' => 'required',
+                  'channel_based' => false,
+                  'locale_based' => true
+              ],  [
+                  'name' => 'sort',
+                  'title' => 'admin::app.admin.system.sort_order',
+                  'type' => 'select',
+                  'options' => [
+                      [
+                          'title' => '1',
+                          'value' => 1
+                      ], [
+                          'title' => '2',
+                          'value' => 2
+                      ], [
+                          'title' => '3',
+                          'value' => 3
+                      ], [
+                          'title' => '4',
+                          'value' => 4
+                      ]
+                  ],
+              ]
           ],
           'validation' => 'required'
         ]
