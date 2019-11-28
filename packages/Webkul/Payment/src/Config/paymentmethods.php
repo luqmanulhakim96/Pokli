9a568783c34c4c14a8b08d3da4ca7b0eee5e71d3
luqmanulhakim96 <hakim9608@gmail.com>
2019-11-28 17:53:51 +0800

    Update paymentmethods.php

--- packages/Webkul/Payment/src/Config/paymentmethods.php
+++ packages/Webkul/Payment/src/Config/paymentmethods.php
@@ -33,7 +33,7 @@ return [
         'code' => 'billplz',
         'title' => 'FPX',
         'description' => 'Bank Transfer',
-        'class' => 'Artanis\Payment\Payment\BillPlz',
+        'class' => 'Artanis\BillPlz\Payment\BillPlz',
         'sandbox' => true,
         'active' => true,
         'sort' => 4
