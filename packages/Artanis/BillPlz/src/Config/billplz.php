e01ed714197a35dbbd3cdaea3b5067839c21d845
luqmanulhakim96 <hakim9608@gmail.com>
2019-11-29 14:42:32 +0800

    error

--- packages/Artanis/BillPlz/src/Config/billplz.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'api_key' => env('BILLPLZ_API_KEY'),
-    'version' => env('BILLPLZ_VERSION', 'v3'),
-    'x-signature' => env('BILLPLZ_X_SIGNATURE'),
-    'sandbox' => env('BILLPLZ_SANDBOX', true),
-    ]
-];
