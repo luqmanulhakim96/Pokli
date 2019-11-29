8141425bcc9241b24139ca5dc3badf1b68411973
luqmanulhakim96 <hakim9608@gmail.com>
2019-11-29 16:30:09 +0800

    Update redirect.blade.php

--- packages/Artanis/BillPlz/src/Resources/views/redirect.blade.php
+++ packages/Artanis/BillPlz/src/Resources/views/redirect.blade.php
@@ -8,14 +8,14 @@
 
   $billplzCreate = Client::make('155994cc-37ea-4c78-9460-1062df930f2c', 'S-b4db8m12r7Te8JmS9O79Rg')->useSandbox();
   $billplzCreate->create(
-    'collection_id'        => 'x7afhxzc',
+    'collection_id'        => "x7afhxzc",
     'email'                => $billingAddress->email,
     'name'                 => $billingAddress->first_name,
     'amount'               => $cart->grand_total,
     'callback_url'         => route('billplz.cancel'),
-    'description'          => 'Testing API',
+    'description'          => "Testing API",
     'redirect_url'         => route('billplz.redirect'),
-    'reference_1_label'    => 'Item : ',
+    'reference_1_label'    => "Item : ",
     'reference_1'          => core()->getCurrentChannel()->name
   );
 
