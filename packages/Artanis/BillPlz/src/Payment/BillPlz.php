90c74d710fc20a525b328cb035f2ca4cbc858ea3
luqmanulhakim96 <hakim9608@gmail.com>
2019-12-02 10:20:37 +0800

    Update BillPlz.php

--- packages/Artanis/BillPlz/src/Payment/BillPlz.php
+++ packages/Artanis/BillPlz/src/Payment/BillPlz.php
@@ -70,8 +70,8 @@ class BillPlz extends Payment
         );
 
         // $id = $response->id;
-        $id = 'kpfjespa';
-
-      return 'https://www.billplz-sandbox.com/api/v3/bills/'.$id;
+        // $id = 'kpfjespa';
+        var_dump($response->toArray());
+      // return 'https://www.billplz-sandbox.com/api/v3/bills/'.$id;
     }
 }
