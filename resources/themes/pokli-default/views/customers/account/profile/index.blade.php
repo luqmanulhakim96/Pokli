a6007201f2e82a3f23d2fd8aeff6f6e001dc199a
Rizal <rizal@artaniscloud.com>
2019-12-02 14:36:09 +0800

    Test

--- resources/themes/pokli-default/views/customers/account/profile/index.blade.php
+++ resources/themes/pokli-default/views/customers/account/profile/index.blade.php
@@ -57,12 +57,12 @@
 
                     <tr>
                         <td>{{ __('shop::app.customer.account.profile.upline-name') }}</td>
-                        <td>{{ $uplineDetails->first_name }} {{ $uplineDetails['last_name'] }}</td>
+                        <td>{{ $uplineDetails->first_name }} {{ $uplineDetails->last_name }}</td>
                     </tr>
 
                     <tr>
                         <td>{{ __('shop::app.customer.account.profile.upline-email') }}</td>
-                        <td>{{ $uplineDetails['email'] }}</td>
+                        <td>{{ $uplineDetails->email }}</td>
                     </tr>
 
                     <tr>
