3e5b555c87df0a3c33093479904ced1d18f097b8
luqmanulhakim96 <hakim9608@gmail.com>
2019-12-05 14:39:40 +0800

    Add livePrice package

--- packages/Artanis/LivePrice/src/Resources/views/home/liveprice.blade.php
+++ packages/Artanis/LivePrice/src/Resources/views/home/liveprice.blade.php
@@ -1,7 +1,7 @@
 <section class="featured-products">
 
     <div class="featured-heading">
-        Live Price<br/>
+        {{ __('shop::app.home.new-products') }}<br/>
 
         <span class="featured-seperator" style="color:lightgrey;">_____</span>
     </div>
