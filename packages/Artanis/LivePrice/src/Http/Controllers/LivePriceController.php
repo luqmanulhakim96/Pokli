a6c4c309190c1f9283b9ce68fe2437eeaac0e0f8
luqmanulhakim96 <hakim9608@gmail.com>
2019-12-05 17:09:03 +0800

    add live update price

---
+++ packages/Artanis/LivePrice/src/Http/Controllers/LivePriceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Artanis\LivePrice\Http\Controllers;
+
+use Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+use Webkul\Customer\Models\Customer;
+use Webkul\Customer\Repositories\CustomerRepository;
+use Webkul\Product\Repositories\ProductReviewRepository;
+
+class LivePriceController extends Controller
+{
+
+    protected $gap;
+
+    public function index()
+    {
+      $gap = DB::table('gold_live_price_gap')->select('last_updated','gram','price')->get();
+
+     return view('liveprice')->with('gold_live_price_gap', $gap);
+    }
+
+}
