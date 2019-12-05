4fa0880462de7803ab99d60fece11c9dab8cb017
luqmanulhakim96 <hakim9608@gmail.com>
2019-12-05 17:38:06 +0800

    Update liveprice.blade.php

--- packages/Artanis/LivePrice/src/Resources/views/home/liveprice.blade.php
+++ packages/Artanis/LivePrice/src/Resources/views/home/liveprice.blade.php
@@ -16,12 +16,12 @@
           			<div id="section-1" class="col-md-3 col-sm-6 col-xs-12" style="display: block;height: 533px;">
                   <div class="live-price-div2" id="gap-div-table2">
             				<div class="gold-live-price-title-table2" id="gap-div-title2">
-            					<a href="{{ route('gapsap.index') }}" target="_BLANK"><img src="/images/liveprice/gap.png" alt="Gold Program" width="245px" height="15px" style="position: relative; top:-4px;"></a>
+            					<a href="{{ route('gapsap.index') }}" target="_BLANK"><img src="" alt="Gold Program" width="245px" height="15px" style="position: relative; top:-4px;"></a>
             				</div>
             					<table class="gold-live-price-table2" id="gap-table-content2">
-                        
+
                     </table>
-                       <a href="{{ route('gapsap.index') }}"><img style="display:block; width:230px; margin: 10px;" src="/images/liveprice/BuyGAP.png"></a>
+                       <a href="{{ route('gapsap.index') }}"><img style="display:block; width:230px; margin: 10px;" src=""></a>
     			        </div>
           			<div class="live-price-bottom-label2" id="live-price-bottom-label2">
                       <span style="font-style:italic;line-height:1; "><br><br>
