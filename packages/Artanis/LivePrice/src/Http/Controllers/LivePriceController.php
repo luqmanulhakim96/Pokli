452c84629640d18ef9775752a64bee0b30e8ec8e
luqmanulhakim96 <hakim9608@gmail.com>
2019-12-05 17:47:07 +0800

    test

--- packages/Artanis/LivePrice/src/Http/Controllers/LivePriceController.php
+++ packages/Artanis/LivePrice/src/Http/Controllers/LivePriceController.php
@@ -18,7 +18,8 @@ class LivePriceController extends Controller
     public function index()
     {
       $gap = DB::table('gold_live_price_gap')->select('last_updated','gram','price')->get();
-       return view('liveprice')->with('gold_live_price_gap', $gap);
+       // return view('liveprice')->with('gold_live_price_gap', $gap);
+       return view($this->_config['view'], compact(['gap']));
     }
 
 }
