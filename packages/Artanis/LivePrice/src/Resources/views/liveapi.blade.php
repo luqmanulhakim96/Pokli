9a300e0d5872f19eef8832de172acc109cf27840
luqmanulhakim96 <hakim9608@gmail.com>
2019-12-06 15:26:18 +0800

    Update liveapi.blade.php

--- packages/Artanis/LivePrice/src/Resources/views/liveapi.blade.php
+++ packages/Artanis/LivePrice/src/Resources/views/liveapi.blade.php
@@ -1,7 +1,7 @@
-@foreach ($gold_live_price_gap as $gaps)
+<!-- @foreach ($gold_live_price_gap as $gaps)
                         <tr>
                         <td>{{ $gaps->last_updated }}</td>
                         <td>{{ $gaps->gram }}</td>
                         <td>{{ $gaps->price }}</td>
                         </tr>
-                        @endforeach
+                        @endforeach -->
