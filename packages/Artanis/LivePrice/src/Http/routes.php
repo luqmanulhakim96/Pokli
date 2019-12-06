6bc77086755e8966fcbd443df8fcf05502d8049c
luqmanulhakim96 <hakim9608@gmail.com>
2019-12-06 11:22:55 +0800

    Update routes.php

--- packages/Artanis/LivePrice/src/Http/routes.php
+++ packages/Artanis/LivePrice/src/Http/routes.php
@@ -2,7 +2,7 @@
 Route::group(['middleware' => ['web']], function () {
     // Route::view('/purchase', 'gapsap::index');
 
-    Route::get('/home/liveprice', 'Artanis\GapSap\Http\Controllers\LivePriceController@index')->defaults('_config', [
+    Route::get('/home/liveprice', 'Artanis\LivePrice\Http\Controllers\LivePriceController@index')->defaults('_config', [
         'view' => 'liveprice::index'
     ])->name('liveprice.index');
 });
