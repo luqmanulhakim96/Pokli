4c40150ec2cdc976490952af22f4ab03767817d1
luqmanulhakim96 <hakim9608@gmail.com>
2019-12-09 15:59:11 +0800

    Update liveapi.blade.php

--- packages/Artanis/LivePrice/src/Resources/views/liveapi.blade.php
+++ packages/Artanis/LivePrice/src/Resources/views/liveapi.blade.php
@@ -5,20 +5,38 @@
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
      }
     $getGAP = "SELECT * FROM gold_live_price_gap";
+    $getGold24k = "SELECT * FROM gold_live_price_24k";
 
-    $queryGAP= mysqli_query($db,$getGAP)or Die("Sorry, dead query.44");
+    $getSAP = "SELECT * FROM silver_live_price_sap";
+    $getSilver24k = "SELECT * FROM silver_live_price_24k";
+
+    $queryGAP= mysqli_query($db,$getGAP)or Die("Sorry, dead query");
     while($row = mysqli_fetch_array($queryGAP))
     {
-        $data[]=$row;
+        $dataGAP[]=$row;
+    } #end of while
+
+    $querySAP= mysqli_query($db,$getSAP)or Die("Sorry, dead query");
+    while($row = mysqli_fetch_array($querySAP))
+    {
+        $dataSAP[]=$row;
     } #end of while
 
  ?>
 <body>
-      @foreach($data as $key => $value)
+      @foreach($dataGAP as $key => $value)
         <tr>
           <th>{{$value["last_updated"]}}</th>
           <th>{{$value["gram"]}}</th>
-          <th>{{$value["price"]}}</th>            
+          <th>{{$value["price"]}}</th>
         </tr>
     @endforeach
+
+    @foreach($dataSAP as $key => $value)
+      <tr>
+        <th>{{$value["last_updated"]}}</th>
+        <th>{{$value["gram"]}}</th>
+        <th>{{$value["price"]}}</th>
+      </tr>
+  @endforeach
 </body>
