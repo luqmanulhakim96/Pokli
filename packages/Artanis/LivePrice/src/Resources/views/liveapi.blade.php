b0bbebf968fc12999cc3b7fefa2d6582eb47dbb2
luqmanulhakim96 <hakim9608@gmail.com>
2019-12-09 15:43:17 +0800

    Update liveapi.blade.php

---
+++ packages/Artanis/LivePrice/src/Resources/views/liveapi.blade.php
@@ -0,0 +1,24 @@
+<?php
+    $db = mysqli_connect("localhost","root","P@ssw0rd123","live_price_api");
+    if (mysqli_connect_errno())
+    {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+     }
+    $getGAP = "SELECT * FROM gold_live_price_gap";
+
+    $queryGAP= mysqli_query($db,$getGAP)or Die("Sorry, dead query.44");
+    while($row = mysqli_fetch_array($queryGAP))
+    {
+        $data[]=$row;
+    } #end of while
+
+ ?>
+<body>
+      @foreach($data as $key => $value)
+        <tr>
+          <th>{{$value->last_updated}}</th>
+          <th>{{$value->gram}}</th>
+          <th>{{$value->price}}</th>            
+        </tr>
+    @endforeach
+</body>
