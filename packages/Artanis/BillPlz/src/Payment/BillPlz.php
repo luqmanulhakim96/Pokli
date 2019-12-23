a451e79116e042039a6ce406d0434f594bd74ad4
luqmanulhakim96 <hakim9608@gmail.com>
2019-12-23 12:55:58 +0800

    update billplz

--- packages/Artanis/BillPlz/src/Payment/BillPlz.php
+++ packages/Artanis/BillPlz/src/Payment/BillPlz.php
@@ -18,7 +18,7 @@ class BillPlz extends Payment
 
     public function __construct()
     {
-        $this->billplz = Client::make('9e044b22-afda-4245-ba20-a9c4249d5cc2', 'S-Fq-j4wtQghYPQ80vd0kjbw');
+        $this->billplz = Client::make('155994cc-37ea-4c78-9460-1062df930f2c', 'S-b4db8m12r7Te8JmS9O79Rg');
         $this->billplz->useSandbox();
     }
     public static function make()
@@ -38,7 +38,7 @@ class BillPlz extends Payment
         $item = $this->getCartItems();
 
         $fields = [
-            'collection_id'        => 'wf6m9pmq',
+            'collection_id'        => 'x7afhxzc',
             'email'                => $billingAddress->email,
             'name'                 => $billingAddress->first_name.' '.$billingAddress->last_name,
             'amount'               => $cart->grand_total,
@@ -58,15 +58,16 @@ class BillPlz extends Payment
         $billingAddress = $cart->billing_address;
         $item = $this->getCartItems();
 
-        $billplzCreate = Client::make('9e044b22-afda-4245-ba20-a9c4249d5cc2', 'S-Fq-j4wtQghYPQ80vd0kjbw')->useSandbox();
+        $billplzCreate = Client::make('155994cc-37ea-4c78-9460-1062df930f2c', 'S-b4db8m12r7Te8JmS9O79Rg')->useSandbox();
         $bill = $billplzCreate->bill();
         $response = $bill->create(
-            'wf6m9pmq', //collection id
+            'x7afhxzc', //collection id
             $billingAddress->email, //user email
             null,
             $billingAddress->first_name.' '.$billingAddress->last_name, //user name
             \Duit\MYR::given($cart->grand_total*100), //total price
             ['callback_url' => route('billplz.verify'), 'redirect_url' => route('billplz.verify')], //url
+            core()->getCurrentChannel()->name //item name
         );
         $responseArray = $response->toArray();
         $url = $responseArray['url'];
