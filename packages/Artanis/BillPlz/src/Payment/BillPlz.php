d84dd450755d4eee828222dcb43d8af399107067
luqmanulhakim96 <hakim9608@gmail.com>
2019-12-24 16:19:46 +0800

    Update BillPlz.php

--- packages/Artanis/BillPlz/src/Payment/BillPlz.php
+++ packages/Artanis/BillPlz/src/Payment/BillPlz.php
@@ -66,7 +66,8 @@ class BillPlz extends Payment
             null,
             $billingAddress->first_name.' '.$billingAddress->last_name, //user name
             \Duit\MYR::given($cart->grand_total*100), //total price
-            ['callback_url' => route('billplz.verify'), 'redirect_url' => route('billplz.verify')]
+            ['callback_url' => route('billplz.verify'), 'redirect_url' => route('billplz.verify')],
+            core()->getCurrentChannel()->name
         );
         $responseArray = $response->toArray();
         $url = $responseArray['url'];
