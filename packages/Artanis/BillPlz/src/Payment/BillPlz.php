9ecc10f58c6d7d6cbf66342f49a024062a62309f
luqmanulhakim96 <hakim9608@gmail.com>
2020-01-31 18:13:02 +0800

    Update BillPlz.php

--- packages/Artanis/BillPlz/src/Payment/BillPlz.php
+++ packages/Artanis/BillPlz/src/Payment/BillPlz.php
@@ -97,7 +97,7 @@ class BillPlz extends Payment
         );
         $responseArray = $response->toArray();
         $url = $responseArray['url'];
-        return redirect()->away($url);
+        return redirect()->away('$url');
         // dd($url);
         // return redirect($url);
       // return 'https://billplz-staging.herokuapp.com/bills/'.$id;
