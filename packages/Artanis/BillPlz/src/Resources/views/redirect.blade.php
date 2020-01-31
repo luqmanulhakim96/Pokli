c9c04b14a8101f506937ce10cac6061e3d655c6f
luqmanulhakim96 <hakim9608@gmail.com>
2020-01-31 18:09:25 +0800

    Update redirect.blade.php

--- packages/Artanis/BillPlz/src/Resources/views/redirect.blade.php
+++ packages/Artanis/BillPlz/src/Resources/views/redirect.blade.php
@@ -3,17 +3,6 @@
 <body data-gr-c-s-loaded="true" cz-shortcut-listen="true">
     You will be redirected to the FPX website in a few seconds.
 
-
-    <form action="{{ $billplz->getBillPlzlUrl() }}" id="billplz_checkout" method="POST">
-        <input value="Click here if you are not redirected within 10 seconds..." type="submit">
-
-        @foreach ($billplz->getFormFields() as $name => $value)
-
-            <input type="hidden" name="{{ $name }}" value="{{ $value }}">
-
-        @endforeach
-    </form>
-
     <script type="text/javascript">
         document.getElementById("billplz_checkout").submit();
     </script>
