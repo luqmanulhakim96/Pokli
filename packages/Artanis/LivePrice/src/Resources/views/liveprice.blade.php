72ab11717696ebc2c588507ef30454147cc93fe3
luqmanulhakim96 <hakim9608@gmail.com>
2020-02-03 18:00:27 +0800

    Update liveprice.blade.php

--- packages/Artanis/LivePrice/src/Resources/views/liveprice.blade.php
+++ packages/Artanis/LivePrice/src/Resources/views/liveprice.blade.php
@@ -8,7 +8,7 @@
     $getLast = "SELECT DATE_FORMAT(`last_updated`, '%d-%M-%Y %H:%i:%S') as dateLast FROM `gold_live_price_gap` GROUP BY `last_updated`";
 
     $getGAP = "SELECT * FROM gold_live_price_gap";
-    $getGold24kBuy = "SELECT * FROM gold_live_price_24k WHERE buy = 'null' ORDER BY gram ASC";
+    $getGold24kBuys = "SELECT * FROM gold_live_price_24k WHERE buy = 'null' ORDER BY gram ASC";
     $getGold24k = "SELECT * FROM gold_live_price_24k ORDER BY gram ASC";
 
     $getSAP = "SELECT * FROM silver_live_price_sap";
@@ -33,7 +33,7 @@
         $dataGold24k[]=$row;
     } #end of while
 
-    $queryGold24kBuy= mysqli_query($db,$getGold24kBuy)or Die("Sorry, dead query");
+    $queryGold24kBuy= mysqli_query($db,$getGold24kBuys)or Die("Sorry, dead query");
     while($row = mysqli_fetch_array($queryGold24kBuy))
     {
         $dataGold24kBuy[]=$row;
