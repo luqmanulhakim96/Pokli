5de81ee12571be038b1b006f05457fe510ea1bed
luqmanulhakim96 <hakim9608@gmail.com>
2020-03-09 17:50:44 +0800

    Update new-invoice.blade.php

--- packages/Webkul/Shop/src/Resources/views/emails/sales/new-invoice.blade.php
+++ packages/Webkul/Shop/src/Resources/views/emails/sales/new-invoice.blade.php
@@ -123,9 +123,7 @@
               </tr>
               @foreach ($invoice->items as $item)
               <tr>
-                  <!-- <td align="center" style="width: 20%;border: 1px solid black;">
-                  <pre style="font-size: 10px">@foreach($serial_number as $key => $data){{ $data->serial_number }}<br>@endforeach</pre>
-                  </td> -->
+                <!-- serial number here -->
                   <td align="center" style="width: 20%;border: 1px solid black;">
                   <pre style="font-size: 10px"><br></pre>
                   </td>
