88ebc216f876fecbfc212ba4794f4af113df9cdb
luqmanulhakim96 <hakim9608@gmail.com>
2020-03-10 17:38:58 +0800

    changes

--- packages/Webkul/Shop/src/Resources/views/emails/layouts/master.blade.php
+++ packages/Webkul/Shop/src/Resources/views/emails/layouts/master.blade.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500" rel="stylesheet" type="text/css">
-        
+
         <style type="text/css">
             @page {
                 margin: 0px;
