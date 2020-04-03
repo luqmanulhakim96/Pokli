175c150eed1cd7942197aa57b6b45b220e7a9662
luqmanulhakim96 <hakim9608@gmail.com>
2020-04-03 21:45:26 +0800

    add branch migration

--- database/migrations/2020_04_03_214452_add_pokli_branch_into_customers.php
+++ database/migrations/2020_04_03_214452_add_pokli_branch_into_customers.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddBranchIntoCustomers extends Migration
+class AddPokliBranchIntoCustomers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,9 @@ class AddBranchIntoCustomers extends Migration
      */
     public function up()
     {
-        //
+      Schema::table('customers', function (Blueprint $table) {
+          $table->string('branch')->nullable();
+      });
     }
 
     /**
@@ -23,6 +25,8 @@ class AddBranchIntoCustomers extends Migration
      */
     public function down()
     {
-        //
+      Schema::table('customers', function (Blueprint $table) {
+          $table->dropColumn('branch');
+      });
     }
 }
