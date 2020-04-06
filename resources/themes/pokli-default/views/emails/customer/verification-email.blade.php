69168c6b362ecd7eeed14c03ef1acfcfddce5b13
luqmanulhakim96 <hakim9608@gmail.com>
2020-04-06 13:32:22 +0800

    Update verification-email.blade.php

--- resources/themes/pokli-default/views/emails/customer/verification-email.blade.php
+++ resources/themes/pokli-default/views/emails/customer/verification-email.blade.php
@@ -2,9 +2,7 @@
 
     <div>
         <div style="text-align: center;">
-            <a href="{{ config('app.url') }}">
                 @include ('shop::emails.layouts.logo')
-            </a>
         </div>
 
         <div  style="font-size:16px; color:#242424; font-weight:600; margin-top: 60px; margin-bottom: 15px">
