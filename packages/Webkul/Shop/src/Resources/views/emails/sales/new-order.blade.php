de34197b17ffc46d7af855e00e7a4b4e9a32ddf6
luqmanulhakim96 <hakim9608@gmail.com>
2020-04-10 13:47:57 +0800

    update new order email

--- packages/Webkul/Shop/src/Resources/views/emails/sales/new-order.blade.php
+++ packages/Webkul/Shop/src/Resources/views/emails/sales/new-order.blade.php
@@ -29,7 +29,7 @@
         </div>
 
         <div style="display: flex;flex-direction: row;margin-top: 20px;justify-content: space-between;margin-bottom: 40px;">
-            @if ($order->shipping_address)
+            <!-- @if ($order->shipping_address)
                 <div style="line-height: 25px;">
                     <div style="font-weight: bold;font-size: 16px;color: #242424;">
                         {{ __('shop::app.mail.order.shipping-address') }}
@@ -61,38 +61,45 @@
                         {{ $order->shipping_title }}
                     </div>
                 </div>
-            @endif
+            @endif -->
 
             <div style="line-height: 25px;">
-                <div style="font-weight: bold;font-size: 16px;color: #242424;">
+                <!-- <div style="font-weight: bold;font-size: 16px;color: #242424;">
                     {{ __('shop::app.mail.order.billing-address') }}
+                </div> -->
+                <div>
+                  {{ __('shop::app.mail.order.order-number') }} {{ $order->id }}
                 </div>
 
                 <div>
-                    {{ $order->billing_address->name }}
+                  {{ __('shop::app.mail.order.order-date') }} {{ $order->created_at }}
                 </div>
 
                 <div>
-                    {{ $order->billing_address->address1 }}, {{ $order->billing_address->state }}
+                  {{ __('shop::app.mail.order.customer-name') }}  {{ $order->billing_address->name }}
                 </div>
 
                 <div>
-                    {{ core()->country_name($order->billing_address->country) }} {{ $order->billing_address->postcode }}
+                    {{ $order->customer->ic }}
                 </div>
 
                 <div>---</div>
 
-                <div style="margin-bottom: 40px;">
-                    {{ __('shop::app.mail.order.contact') }} : {{ $order->billing_address->phone }}
+                <div>
+                      {{ __('shop::app.mail.order.customer-tel') }} {{ $order->customer->phone }}
                 </div>
 
-                <div style="font-size: 16px; color: #242424;">
+                <div style="margin-bottom: 40px;">
+                      {{ __('shop::app.mail.order.customer-email') }} {{ $order->customer->email }}
+                </div>
+
+                <!-- <div style="font-size: 16px; color: #242424;">
                     {{ __('shop::app.mail.order.payment') }}
                 </div>
 
                 <div style="font-weight: bold;font-size: 16px; color: #242424;">
                     {{ core()->getConfigData('sales.paymentmethods.' . $order->payment->method . '.title') }}
-                </div>
+                </div> -->
             </div>
         </div>
 
