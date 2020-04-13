ac07ac5c9d8a01645074902ca2d9d91fd0076e84
luqmanulhakim96 <hakim9608@gmail.com>
2020-04-13 14:31:56 +0800

    Update NewPurchaseGAPNotification.php

--- packages/Artanis/GapSap/src/Mail/NewPurchaseGAPNotification.php
+++ packages/Artanis/GapSap/src/Mail/NewPurchaseGAPNotification.php
@@ -16,12 +16,12 @@ class NewPurchaseGAPNotification extends Mailable
      *
      * @return void
      */
-     public $purchase;
+     public $history;
 
 
      public function __construct($purchase)
      {
-       $this->purchase = $purchase;
+       $this->history = $history;
 
      }
 
@@ -32,7 +32,7 @@ class NewPurchaseGAPNotification extends Mailable
       */
      public function build()
      {
-       return $this->to($this->purchase->customer_email, $this->purchase->customer_full_name)
+       return $this->to($this->history->customer_email, $this->history->customer_full_name)
                ->from(env('SHOP_MAIL_FROM'))
                ->subject(trans('shop::app.mail.order.subject'))
                ->view('shop::emails.sales.new-order');
