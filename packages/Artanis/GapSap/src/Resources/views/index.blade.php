916fb3f55714e8b337fd7e5f54eb99897c5bd516
luqmanulhakim96 <hakim9608@gmail.com>
2020-06-04 12:38:07 +0800

    re-enable FPX in MYUNCANG PURCHASE

--- packages/Artanis/GapSap/src/Resources/views/index.blade.php
+++ packages/Artanis/GapSap/src/Resources/views/index.blade.php
@@ -28,7 +28,7 @@
 
                     <select id="mode_of_payment" name="mode_of_payment" class="control" v-validate="'required'" data-vv-as="&quot;{{ __('gapsap::app.purchase.form-mode-of-payment') }}&quot;">
                         <option value="">Please Select Payment Method</option>
-                        <!-- <option value="fpx">FPX</option> -->
+                        <option value="fpx">FPX</option>
                         <option value="bankin">Bank In</option>
                     </select>
                     <span class="control-error" v-if="errors.has('mode_of_payment')">@{{ errors.first('mode_of_payment') }}</span>
@@ -144,7 +144,7 @@
 
                                     <select name="mode_of_payment" class="control" v-validate="'required'" data-vv-as="&quot;{{ __('gapsap::app.purchase.form-mode-of-payment') }}&quot;">
                                         <option value="">Please Select Payment Method</option>
-                                        <!-- <option value="fpx">FPX</option> -->
+                                        <option value="fpx">FPX</option>
                                         <option value="bankin">Bank In</option>
                                     </select>
                                     <span class="control-error" v-if="errors.has('mode_of_payment')">@{{ errors.first('mode_of_payment') }}</span>
